81e0a4e9c4c716c02507a6231ec2953e01f39d3e
popovic-olivera <olja997@hotmail.com>
2020-08-27 21:58:58 +0200

    Created restore password form and removed test dialog open in register form

--- frontend-server/src/app/restore-password/restore-password.component.ts
+++ frontend-server/src/app/restore-password/restore-password.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthenticationService } from '../authentication.service';
+import { passwordValidator, passwordsEqual } from '../register/custom.validators';
 
 @Component({
   selector: 'app-restore-password',
   templateUrl: './restore-password.component.html',
   styleUrls: ['./restore-password.component.css']
 })
-export class RestorePasswordComponent implements OnInit {
 
-  constructor() { }
+export class RestorePasswordComponent implements OnInit {
+  public resetPasswordForm: FormGroup;
+  public hidePassword = true;
+
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.resetPasswordForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email])
+    });
   }
 
+  get email() { return this.resetPasswordForm.get('email'); }
+
+  emailErrorMessage(): string {
+    if (this.email.hasError('required')) {
+      return 'Ovo je obavezno polje';
+    } 
+    else if (this.email.hasError('email')) {
+      return 'Ovo nije ispravna imejl adresa';
+    } 
+    else {
+      return 'Došlo je do greške';
+    }
+  }
+
+  resetEnabled(): boolean {
+    return this.resetPasswordForm.valid;
+  }
+
+  async onSubmit() {
+    // TODO restore password in database
+  }
 }
