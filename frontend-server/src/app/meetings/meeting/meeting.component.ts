aae432154baca7ed71e1c7ac41398a02c12fb627
alex-x-o <mi16048@alas.matf.bg.ac.rs>
2020-09-06 01:07:35 +0200

    Made meeting component responsive to viewport change

--- frontend-server/src/app/meetings/meeting/meeting.component.ts
+++ frontend-server/src/app/meetings/meeting/meeting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, ViewChild, ElementRef, AfterViewInit, Renderer2, HostListener } from '@angular/core';
 import { Meeting } from '../meeting.model';
 import { Button } from './button.model';
 import { EventEmitter } from '@angular/core';
@@ -10,16 +10,21 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   templateUrl: './meeting.component.html',
   styleUrls: ['./meeting.component.css']
 })
-export class MeetingComponent implements OnInit {
+export class MeetingComponent implements OnInit, AfterViewInit {
 
-  @Input() 
+  @ViewChild('image')
+  elem: ElementRef;
+
+  @Input()
   public meeting: Meeting;
   public buttons: Button[] = [];
 
   @Output()
   public emitMeetingToDelete: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor(private router: Router, public auth: AuthenticationService) { }
+  constructor(private router: Router,
+              public auth: AuthenticationService,
+              private renderer: Renderer2) { }
 
   ngOnInit(): void {
     if (!this.meeting.authorImage) {
@@ -30,9 +35,26 @@ export class MeetingComponent implements OnInit {
                   [this.meeting.surveyUrl, 'surveyUrl'],
                   [this.meeting.videoUrl, 'videoUrl'],
                   [this.meeting.presentationUrl, 'presentationUrl']];
-                  
-    this.buttons = urls.filter((url) => { if (url[0] !== null) return true; })
-                       .map((url) => { return new Button(url[0], url[1]); });
+
+    this.buttons = urls.filter((url) => { if (url[0] !== null) {return true; }})
+                       .map((url) => new Button(url[0], url[1]));
+  }
+
+  ngAfterViewInit(): void {
+    if (window.innerWidth < 960) {
+      this.unsetRadius();
+    } else {
+      this.setRadius();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (event.target.innerWidth < 960) {
+      this.unsetRadius();
+    } else {
+      this.setRadius();
+    }
   }
 
   public goToAuthorPage() {
@@ -43,5 +65,14 @@ export class MeetingComponent implements OnInit {
 
   public onDeleteMeeting() {
     this.emitMeetingToDelete.emit(this.meeting._id);
+  }
+
+  setRadius(): void {
+    this.renderer.setStyle(this.elem.nativeElement.querySelector('img'), 'border-radius', '20px');
+  }
+
+  unsetRadius(): void {
+    this.renderer.setStyle(this.elem.nativeElement.querySelector('img'), 'border-top-left-radius', '0px');
+    this.renderer.setStyle(this.elem.nativeElement.querySelector('img'), 'border-top-right-radius', '0px');
   }
 }
