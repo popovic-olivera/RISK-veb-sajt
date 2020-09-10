1ec02fe2dfee47976763c479912b679d3b662bf2
popovic-olivera <olja997@hotmail.com>
2020-09-10 14:32:15 +0200

    Fixed bug with blog post list

--- frontend-server/src/app/blog/blog/blog.component.ts
+++ frontend-server/src/app/blog/blog/blog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../blog.service';
 import { BlogPost } from '../blog-post.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-blog',
@@ -11,13 +12,14 @@ import { BlogPost } from '../blog-post.model';
 export class BlogComponent implements OnInit {
   public blogPosts: BlogPost[];
 
-  constructor(private blogService: BlogService) { 
+  constructor(private blogService: BlogService, private dataService: DataService) { 
     this.blogService.getBlogPosts().subscribe(
       data => this.blogPosts = data.reverse()
     );
   }
 
   ngOnInit(): void {
+    this.dataService.changeMessage('blog');
   }
 
 }
